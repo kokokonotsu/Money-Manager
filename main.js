a4b72143a9f111143e90aef758e830ebb20788f6
Ethan Simpson <ethansown@gmail.com>
2018-11-26 16:23:28 -0600

    Added Event Handlers for Modal

---
+++ main.js
@@ -0,0 +1,10 @@
+var expenseModal = document.getElementById("expense-modal");
+var expenseModalButton = document.getElementById("create-expense-modal-button");
+var expenseClose = document.getElementById("expense-close");
+
+//Add Event Listener for whenever user clicks "Add Expense" button
+expenseModalButton.addEventListener("click", () => { expenseModal.style.display = "block"; });
+//Add Event Listener for whenever user clicks the close button
+expenseClose.addEventListener("click", () => {expenseModal.style.display = "none"; });
+//Add Event Listener for whenever user clicks outside of the modal content to close modal
+window.addEventListener("click", (event) => { if(event.target == expenseModal){ expenseModal.style.display = "none"; }; });
