87649b2de39ebe5997fd2576e634250d3ee6482e
Ethan Simpson <ethansown@gmail.com>
2018-11-27 13:02:10 -0600

    Restructured JS

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var expenseModal = document.getElementById("expense-modal");
 var expenseModalButton = document.getElementById("create-expense-modal-button");
 var expenseClose = document.getElementById("expense-close");
+var incomeModal = document.getElementById("income-modal");
+var incomeModalButton = document.getElementById("create-income-modal-button");
+var incomeClose = document.getElementById("income-close");
 
 //Add Event Listener for whenever user clicks "Add Expense" button
 expenseModalButton.addEventListener("click", () => { expenseModal.style.display = "block"; });
@@ -8,10 +11,6 @@ expenseModalButton.addEventListener("click", () => { expenseModal.style.display 
 expenseClose.addEventListener("click", () => {expenseModal.style.display = "none"; });
 //Add Event Listener for whenever user clicks outside of the modal content to close modal
 window.addEventListener("click", (event) => { if(event.target == expenseModal){ expenseModal.style.display = "none"; }; });
-
-var incomeModal = document.getElementById("income-modal");
-var incomeModalButton = document.getElementById("create-income-modal-button");
-var incomeClose = document.getElementById("income-close");
 
 //Add Event Listener for whenever user clicks "Add Income" button
 incomeModalButton.addEventListener("click", () => { incomeModal.style.display = "block"; });
