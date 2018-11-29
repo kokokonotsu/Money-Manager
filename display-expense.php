80cd8e4ada8468ef9ee946ef4c8c62ae63e58af4
Ethan Simpson <ethansown@gmail.com>
2018-11-29 16:35:04 -0600

    Various Bug Fixes

--- display-expense.php
+++ display-expense.php
@@ -3,7 +3,7 @@
 db();
 //Assign returned connection variable to global variable instance
 global $link;
-$query = "SELECT expenseTitle, expenseDescription, expenseTime, expenseDate, balanceAfterExpense FROM expense";
+$query = "SELECT * FROM expense";
 $result = mysqli_query($link, $query);
 if(mysqli_num_rows($result) >= 1){
   while($row = mysqli_fetch_array($result)){
@@ -11,16 +11,18 @@ if(mysqli_num_rows($result) >= 1){
     $title = $row["expenseTitle"];
     $time = $row["expenseTime"];
     $date = $row["expenseDate"];
-    $expense = $row["balanceAfterExpense"];
+    $expense = $row["expenseTotal"];
     ?>
     <ul>
-      <li>
-      <a href="detail.php?id=<?php echo $id?>" class="task-title"><?php echo "$title"?></a>
-      <span><?php echo "[$time]";?></span>
-      <span><?php echo "[$date]";?></span>
-      <span><?php echo "\$$expense";?>
-      <button type="button" class="edit-button"><a href="update.php?id=<?php echo $id?>"><i class="fas fa-pen"></i></a></button>
-      <button type="button"><a href="delete.php?id=<?php echo $id?>"><i class="fas fa-times"></i></a></button>
+      <li class="li-expense">
+        <a href="detail.php?id=<?php echo $id?>" class="task-title"><?php echo "$title"?></a>
+        <div class="li-expense-span-div">
+          <span><?php echo "\$$expense";?></span>
+          <span><?php echo "[$time]";?></span>
+          <span><?php echo "[$date]";?></span>
+        </div>
+        <button type="button" class="edit-button"><a href="update.php?id=<?php echo $id?>"><i class="fas fa-pen"></i></a></button>
+        <button type="button"><a href="delete.php?id=<?php echo $id?>"><i class="fas fa-times"></i></a></button>
       </li>
     </ul>
     <?php
