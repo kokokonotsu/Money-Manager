1f94f2625a08a9c34060dcc6d6f1a2faf2840cfd
Ethan Simpson <ethansown@gmail.com>
2018-11-29 22:10:58 -0600

    Added and Fixed Add-Expense.php

---
+++ add-expense.php
@@ -0,0 +1,26 @@
+<?php 
+  if(isset($_POST['expense-submit'])){
+    $expenseTitle = $_POST["expenseTitle"];
+    $expenseDescription = $_POST["expenseDescription"];
+    $expenseTotal = $_POST["expenseTotal"];
+    $expenseTime = $_POST["expenseTime"];
+    $expenseDate = $_POST["expenseDate"];
+    db();
+    global $link;
+    $query_expense = "INSERT INTO expense(expenseTitle, expenseDescription, expenseTime, expenseDate) VALUES('$expenseTitle', '$expenseDescription', '$expenseTotal', '$expenseTime', '$expenseDate')";
+    $result_expense = mysqli_query($link, $query_expense);
+    if(!$result_expense){
+      echo mysqli_error();
+    }
+    $balanceExpenseQuery = "SELECT TotalBalance FROM balance";
+    $balanceExpenseResult = mysqli_query($link, $balanceExpenseQuery);
+    $balanceExpenseRow = mysqli_fetch_array($balanceExpenseResult);
+    $balanceExpenseAmount = $balanceExpenseRow["TotalBalance"];
+    $balance_expense = $balanceExpenseAmount - $expenseTotal;
+    $insertBalanceExpense = "REPLACE INTO balance VALUES (1, '$balance_expense')";
+    $insertBalanceExpenseResult = mysqli_query($link, $insertBalanceExpense);
+    if(!$insertBalanceExpenseResult){
+      echo("No Balance Result");
+    }
+  }
+?>
