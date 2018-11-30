78114adfdb46888c692aa2958b0b7bd4cae1b23f
Ethan Simpson <ethansown@gmail.com>
2018-11-29 22:10:41 -0600

    Deleted Unneeded Code

--- insert-income.php
+++
@@ -1,7 +0,0 @@
-$balanceQuery = "SELECT TotalBalance FROM balance";
-$balanceResult = mysqli_query($link, $balanceQuery);
-$balanceRow = mysqli_fetch_array($balanceResult);
-$balanceAmount = $balanceRow["TotalBalance"];
-$balance = $balanceAmount + $incomeTotal;
-$insertBalance = "REPLACE INTO balance VALUES (1, '$balance')";
-mysqli_query($link, $insertBalance);
