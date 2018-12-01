ada7056033ef9d7601a52b9f3188e75c2455fc8a
Ethan Simpson <ethansown@gmail.com>
2018-12-01 15:18:32 -0600

    Changed Name of input

--- edit-income.php
+++ edit-income.php
@@ -2,36 +2,35 @@
     $newIncomeTotal = $_POST["edit-income-total"];
     $newIncomeTime = $_POST["edit-income-time"];
     $newIncomeDate = $_POST["edit-income-date"];
-    $oldIncomeTotal = $_POST["edit-income-form-hidden-total"];
     $editIncomeFormHiddenId = $_POST["edit-income-form-hidden-id"];
-    $newBalanceTotal;
+    $oldIncomeTotal = $_POST["edit-income-form-hidden-total"];
+    $newBalanceIncomeTotal;
     $getBalanceQuery = "SELECT TotalBalance FROM balance";
     $getBalanceQueryResult = mysqli_query($link, $getBalanceQuery);
     $currentBalanceRow = mysqli_fetch_array($getBalanceQueryResult);
-    $currentBalance = $currentBalanceRow["TotalBalance"];
+    $currentBalanceIncome = $currentBalanceRow["TotalBalance"];
     if($newIncomeTotal > $oldIncomeTotal){
         $newBalanceTotalSubtract = ($newIncomeTotal - $oldIncomeTotal);
-        $newBalanceTotal = ($currentBalance - $newBalanceTotalSubtract);
-        $newBalanceSubtractQuery = "UPDATE balance SET TotalBalance = '$newBalanceTotal' WHERE id = 1";
+        $newBalanceIncomeTotal = ($currentBalanceIncome - $newBalanceTotalSubtract);
+        $newBalanceSubtractQuery = "UPDATE balance SET TotalBalance = '$newBalanceIncomeTotal' WHERE id = 1";
         $newBalanceSubtractQueryResult = mysqli_query($link, $newBalanceSubtractQuery);
         if(!$newBalanceSubtractQueryResult){
             echo mysqli_error();
         }
     } else if($newIncomeTotal < $oldIncomeTotal){
         $newBalanceTotalAdd = ($oldIncomeTotal - $newIncomeTotal);
-        $newBalanceTotal = ($currentBalance + $newBalanceTotalAdd);
-        $newBalanceAddQuery = "UPDATE balance SET TotalBalance = '$newBalanceTotal' WHERE id = 1";
+        $newBalanceIncomeTotal = ($currentBalanceIncome + $newBalanceTotalAdd);
+        $newBalanceAddQuery = "UPDATE balance SET TotalBalance = '$newBalanceIncomeTotal' WHERE id = 1";
         $newBalanceAddQueryResult = mysqli_query($link, $newBalanceAddQuery);
         if(!$newBalanceAddQueryResult){
             echo mysqli_error();
         }
     }
     $updateIncomeQuery = "UPDATE income 
-    SET 
-    incomeTotal = '$newIncomeTotal',
+    SET incomeTotal = '$newIncomeTotal',
     incomeTime = '$newIncomeTime',
     incomeDate = '$newIncomeDate'
-    WHERE id = '$editIncomeFormHiddenId'";
+    WHERE id = $editIncomeFormHiddenId";
     $updateIncomeResult = mysqli_query($link, $updateIncomeQuery);
     if(!$updateIncomeResult){
         echo mysqli_error();
--- index.php
+++ index.php
@@ -84,7 +84,7 @@ if(isset($_POST["expense-edit-button"])){
 if(isset($_POST["income-edit-button"])){
   $editIncomeId = $_POST["income-edit-hidden-id"];
   require("edit-income-form.php");
-  if(isset($_POST["edit-income-button"])){
+  if(isset($_POST["edit-income-submit"])){
     require("edit-income.php");
   }
 }
