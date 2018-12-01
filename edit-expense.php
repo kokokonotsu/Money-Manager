7036712464b8c59f098962fbed7072a2069c87c8
Ethan Simpson <ethansown@gmail.com>
2018-12-01 12:30:59 -0600

    Edit Expense now Updates Balance when changing Expense Amount

--- edit-expense.php
+++ edit-expense.php
@@ -4,7 +4,30 @@
     $newExpenseTotal = $_POST["edit-expense-total"];
     $newExpenseTime = $_POST["edit-expense-time"];
     $newExpenseDate = $_POST["edit-expense-date"];
+    $oldExpenseTotal = $_POST["edit-expense-form-hidden-total"];
     $editExpenseFormHiddenId = $_POST["edit-expense-form-hidden-id"];
+    $newBalanceTotal;
+    $getBalanceQuery = "SELECT TotalBalance FROM balance";
+    $getBalanceQueryResult = mysqli_query($link, $getBalanceQuery);
+    $currentBalanceRow = mysqli_fetch_array($getBalanceQueryResult);
+    $currentBalance = $currentBalanceRow["TotalBalance"];
+    if($newExpenseTotal > $oldExpenseTotal){
+        $newBalanceTotalSubtract = ($newExpenseTotal - $oldExpenseTotal);
+        $newBalanceTotal = ($currentBalance - $newBalanceTotalSubtract);
+        $newBalanceSubtractQuery = "UPDATE balance SET TotalBalance = '$newBalanceTotal' WHERE id = 1";
+        $newBalanceSubtractQueryResult = mysqli_query($link, $newBalanceSubtractQuery);
+        if(!$newBalanceSubtractQueryResult){
+            echo mysqli_error();
+        }
+    } else if($newExpenseTotal < $oldExpenseTotal){
+        $newBalanceTotalAdd = ($oldExpenseTotal - $newExpenseTotal);
+        $newBalanceTotal = ($currentBalance + $newBalanceTotalAdd);
+        $newBalanceAddQuery = "UPDATE balance SET TotalBalance = '$newBalanceTotal' WHERE id = 1";
+        $newBalanceAddQueryResult = mysqli_query($link, $newBalanceAddQuery);
+        if(!$newBalanceAddQueryResult){
+            echo mysqli_error();
+        }
+    }
     $updateExpenseQuery = "UPDATE expense 
     SET expenseTitle = '$newExpenseTitle',
     expenseDescription = '$newExpenseDescription',
